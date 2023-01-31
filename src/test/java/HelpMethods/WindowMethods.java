9b54c8b11f154020769db1f2dfb4d5fe2084b520
Irinamarciuc28 <marciuc.irina@yahoo.com>
2023-01-31 19:50:12 +0200

    finish page object model

--- src/test/java/HelpMethods/WindowMethods.java
+++ src/test/java/HelpMethods/WindowMethods.java
@@ -2,7 +2,22 @@ package HelpMethods;
 
 import org.openqa.selenium.WebDriver;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WindowMethods {
 
-    private   WindowMethods(WebDriver)
+    private   WebDriver driver;
+
+    public WindowMethods(WebDriver driver){this.driver = driver; }
+    public void moveSpecificTabWindow(Integer index){
+        List<String> browserTabs=new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(browserTabs.get(index));
+
+    }
+    public void CloseCurrentTabWindow(){
+        driver.close();
+
+    }
+
 }
--- src/test/java/Tests/WindowTest.java
+++ src/test/java/Tests/WindowTest.java
@@ -1,10 +1,12 @@
 package Tests;
 
+import HelpMethods.ElementMethods;
+import HelpMethods.PageMethod;
+import HelpMethods.WindowMethods;
 import ShareData.ShareData;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
@@ -16,13 +18,19 @@ import static org.openqa.selenium.By.cssSelector;
 public class WindowTest extends ShareData {
 
         @Test
-        public void MetodaTest() throws  InterruptedException{
+        public void MetodaTest() {
+            ElementMethods element = new ElementMethods(getDriver());
+            PageMethod pelement = new PageMethod(getDriver());
+            WindowMethods welement = new WindowMethods(getDriver());
+
 
 
             WebElement SkipSignIn = getDriver().findElement(By.id("btn2"));
-            SkipSignIn.click();
+            element.clickElement(SkipSignIn);
 
             WebElement SwitchTo = getDriver().findElement(By.xpath("//a[text()='SwitchTo']"));
+
+
             //Mergem cu mouse pe anumit element
             Actions actions = new Actions(getDriver());
 
@@ -31,15 +39,18 @@ public class WindowTest extends ShareData {
 
             WebElement WindowElement = getDriver().findElement(By.xpath("//a[text()='Windows']"));
             WindowElement.click();
-            getDriver().navigate().to("http://demo.automationtesting.in/Windows.html");
+
+
+            pelement.navigateToPage("http://demo.automationtesting.in/Windows.html");
 
 
 
             List<WebElement> WindowOptions =getDriver().findElements(cssSelector(".nav-stacked>li>a"));
-            WindowOptions.get(0).click();
+            element.clickElement(WindowOptions.get(0));
 
             WebElement TabButton=getDriver().findElement(By.cssSelector("#Tabbed>a>button"));
-            TabButton.click();
+            element.clickElement(TabButton);
+
             //Identificam multimea de taburi/Windows
 
             List <String>Tabs=new ArrayList<>(getDriver().getWindowHandles());
@@ -57,9 +68,9 @@ public class WindowTest extends ShareData {
 
             List<String>Windows=new ArrayList<>(getDriver().getWindowHandles());
             //Ne mutam cu focusul pe un window specific
-            getDriver().switchTo().window(Windows.get(1));
+            welement.moveSpecificTabWindow(1);
             getDriver().close();
-            getDriver().switchTo().window(Windows.get(0));
+            welement.moveSpecificTabWindow(0);
 
             WindowOptions.get(2).click();
             WebElement MultipleButton = getDriver().findElement(By.cssSelector("#Multiple>button"));
